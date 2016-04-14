9c29e502ecf5c420a32777bec6761e238ddf29ea
konglx <2403693382@qq.com>
2016-04-14 21:57:55 +0800

    ss

--- 0414-01.py
+++ 0414-01.py
@@ -7,6 +7,10 @@ def find(value, a_list):
     True
     >>> find(40, [14, 15, 16, 4, 6, 5])
     False
+    >>> find(1, [1])
+    False
+    >>> find(1, [])
+    False
     """
     for i in range(len(a_list))[:-1]:
         for j in range(i+1, len(a_list)):
